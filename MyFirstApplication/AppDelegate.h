830019398656bbfda4b7a88203d563285243acca
Grover Light <grover.light@gmail.com>
2015-04-06 13:24:16 -0700

    first commit

---
+++ MyFirstApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApplication
+//
+//  Created by Grover Light on 4/6/15.
+//  Copyright (c) 2015 learning, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApplication
+//
+//  Created by Grover Light on 4/6/15.
+//  Copyright (c) 2015 learning, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
